293ed5148915ebaa390d0c8d2808147208e71af1
Paulo Gabriel Justino Bezerra <pgj1085@gmail.com>
2021-06-30 14:44:33 -0300

    Update schema file

--- twitch-loco-bot-starter/schemas.sql
+++ twitch-loco-bot-starter/schemas.sql
@@ -1,12 +1,12 @@
-create table users (
+create table if not exists users (
 	username text primary key,
 	join_date timestamp
 );
 
-create table messages (
+create table if not exists messages (
 	id bigserial primary key,
 	username text references users(username),
 	message text not null,
-	channel text not null
+	channel text not null,
 	message_date timestamp
 );
