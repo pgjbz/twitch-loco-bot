458a60dca247a84c4540b22928d0cfb2f59f79ec
Paulo Gabriel <pgj1085@gmail.com>
2021-08-12 14:26:23 -0300

    Removing useless log and key map from chatMessage

--- twitch-loco-lib/src/main/java/com/pgjbz/twitch/loco/model/ChatMessage.java
+++ twitch-loco-lib/src/main/java/com/pgjbz/twitch/loco/model/ChatMessage.java
@@ -3,9 +3,6 @@ package com.pgjbz.twitch.loco.model;
 import lombok.Getter;
 import lombok.ToString;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
 @Getter
@@ -15,7 +12,6 @@ public class ChatMessage {
     private final String message;
     private final String user;
     private final String channel;
-    private final Map<String, String> keys = new HashMap<>();
 
     public ChatMessage(String message, String user, String channel) {
         if(isBlank(message) || isBlank(user) || isBlank(channel))
