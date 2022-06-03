669345dfae1df1866e131a456dec29c03ee85a89
Paulo Gabriel Justino Bezerra <pgjbzr@gmail.com>
2022-06-03 14:58:25 -0300

    reduce ifs comparing 'none' string

--- irc-twitch/src/irc/parser/mod.rs
+++ irc-twitch/src/irc/parser/mod.rs
@@ -9,7 +9,11 @@ const DEFAULT_NONE: &str = "none";
 impl Parser {
     pub(super) fn parse(&self, input: String) -> IrcResult {
         let irc_type = self.extract_event(&input);
-        let channel = self.extract_channel(&input);
+        let channel = if let Some(channel) = self.extract_channel(&input) {
+            channel
+        } else {
+            DEFAULT_NONE.into()
+        };
         let nickname = self.extract_nickname(&input, &irc_type);
         let message = if let IrcType::Message = irc_type {
             self.extract_message(&input)
@@ -21,42 +25,35 @@ impl Parser {
 
     fn extract_event(&self, input: &str) -> IrcType {
         let regex = Regex::new(r"((?<=\s)([A-Z]+))|(([A-Z]+)(?=\s))").unwrap();
-        self.match_regex_string(regex, input, 0).into()
+        if let Some(value) = self.match_regex_string(regex, input, 0) {
+            value.into()
+        } else {
+            IrcType::Unknown
+        }
     }
 
-    fn extract_channel(&self, input: &str) -> String {
+    fn extract_channel(&self, input: &str) -> Option<String> {
         let regex = Regex::new(r"(?<=\s#)(\w+)").unwrap();
         self.match_regex_string(regex, input, 0)
     }
 
     fn extract_nickname(&self, input: &str, irc_type: &IrcType) -> Option<String> {
         let regex = irc_type.display_name();
-
-        let nickname = self.match_regex_string(regex, input, 0);
-        if &nickname[..] == DEFAULT_NONE {
-            None
-        } else {
-            Some(nickname)
-        }
+        self.match_regex_string(regex, input, 0)
     }
 
     fn extract_message(&self, input: &str) -> Option<String> {
         let regex = Regex::new(r"(?<=PRIVMSG\s#)(.+?)(?<=\w\s:)(.+)").unwrap();
-        let message = self.match_regex_string(regex, input, 2);
-        if &message[..] == DEFAULT_NONE {
-            None
-        } else {
-            Some(message)
-        }
+        self.match_regex_string(regex, input, 2)
     }
 
-    fn match_regex_string(&self, regex: Regex, input: &str, idx: usize) -> String {
+    fn match_regex_string(&self, regex: Regex, input: &str, idx: usize) -> Option<String> {
         match regex.captures(input) {
             Ok(Some(cap)) => match cap.get(idx) {
-                Some(m) => m.as_str().into(),
-                None => DEFAULT_NONE.into(),
+                Some(m) => Some(m.as_str().into()),
+                None => None,
             },
-            _ => DEFAULT_NONE.into(),
+            _ => None,
         }
     }
 }
